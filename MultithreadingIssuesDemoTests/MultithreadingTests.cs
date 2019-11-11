557a61d873b9373a5057d53ec67bfad894bf3932
Artem Myronov <49313106+gl-artem-myronov@users.noreply.github.com>
2019-11-11 14:15:02 +0200

    AtomicityDemoTest and RaceConditionDemoTest have been added
    * AtomicityDemoTest - demonstrates issue with thread-unsafe increment for integer variable.
    * RaceConditionDemoTest - demonstrates race condition between thread, which pushes data to collection and thread, which popes data from the data collection without any synchronization.

---
+++ MultithreadingIssuesDemoTests/MultithreadingTests.cs
@@ -0,0 +1,75 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace MultithreadingIssuesDemoTests
+{
+    [TestFixture]
+    public class MultithreadingTests
+    {
+        [Test]
+        [Repeat(2)]
+        public void AtomicityDemoTest()
+        {
+            const int incrementCount = 100000;
+            const int threadCount = 5;
+
+            int sharedVariable = 0;
+
+            void incrementerFunc()
+            {
+                for (int i = 0; i < incrementCount; i++)
+                {
+                    //Interlocked.Increment(ref sharedVariable);
+                    sharedVariable += 1;
+                }
+            }
+
+            var threads = new List<Thread>();
+            for (int i = 0; i < threadCount; i++)
+            {
+                threads.Add(new Thread(incrementerFunc));
+            }
+
+            threads.ForEach(t => t.Start());
+            threads.ForEach(t => t.Join());
+
+            Assert.AreEqual(incrementCount * threadCount, sharedVariable);
+        }
+
+        [Test]
+        [Repeat(5)]
+        public void RaceConditionDemoTest()
+        {
+            const int itemsCount = 100000;
+
+            var queue = new Queue<int>(itemsCount);
+
+            var appendThread = new Thread(() =>
+            {
+                for (int i = 0; i < itemsCount; i++)
+                {
+                    queue.Enqueue(i);
+                }
+            });
+
+            var readThread = new Thread(() =>
+            {
+                do
+                {
+                    queue.Dequeue();
+                }
+                while (queue.Count > 0);
+            });
+
+            appendThread.Start();
+            readThread.Start();
+
+            appendThread.Join();
+            readThread.Join();
+
+            Assert.AreEqual(0, queue.Count);
+        }
+
+    }
+}
